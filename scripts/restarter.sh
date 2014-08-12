ea58252368b5dc0ea699e14ab97df6e5cb375ab0
Ruslan Terekhov <rterehov@gmail.com>
2014-08-12 23:31:29 +0400

    Убираем рестарт чатов

--- scripts/restarter.sh
+++ scripts/restarter.sh
@@ -3,19 +3,10 @@
 ROLE=$1
 BASE='/home/class/projects'
 FILE_UWSGI="$BASE/$ROLE/restart.uwsgi"
-FILE_CHATS="$BASE/$ROLE/restart.chats"
 if [ -f $FILE_UWSGI ]
 then
     rm $FILE_UWSGI
     date
     echo "Restart uwsgi $ROLE"
     service uwsgi restart $ROLE
-fi
-
-if [ -f $FILE_CHATS ]
-then
-    rm $FILE_CHATS
-    date
-    echo "Restart chats $ROLE"
-    svc -t /etc/service/$ROLE.tornado.chat.*
 fi
